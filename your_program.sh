815fccf29d0ac6924235d45793260501df0c3669
Charles EM Reynolds <reynoldscem@gmail.com>
2024-07-24 10:26:19 +0100

    Include vcpkg path

--- your_program.sh
+++ your_program.sh
@@ -12,9 +12,10 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program compiles locally
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
+VCPKG_ROOT="${HOME}/vcpkg"
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
   cmake --build ./build
 )
 
