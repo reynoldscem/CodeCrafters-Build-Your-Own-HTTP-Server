46abe3a21ff7e16ad5e5c138d92ddc36e78ec0c3
Charles EM Reynolds <reynoldscem@gmail.com>
2024-07-24 11:25:35 +0100

    Refactor init code

--- src/server.cpp
+++ src/server.cpp
@@ -10,6 +10,45 @@
 #include <sstream>
 #include <vector>
 
+int create_server_socket(int port) {
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+    std::cerr << "Failed to create server socket\n";
+    return -1;
+  }
+
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+    std::cerr << "setsockopt failed\n";
+    close(server_fd);
+    return -1;
+  }
+
+  return server_fd;
+}
+
+bool bind_socket(int server_fd, int port) {
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(port);
+
+  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+    std::cerr << "Failed to bind to port " << port << "\n";
+    return false;
+  }
+
+  return true;
+}
+
+bool start_listening(int server_fd, int backlog) {
+  if (listen(server_fd, backlog) != 0) {
+    std::cerr << "listen failed\n";
+    return false;
+  }
+  return true;
+}
+
 std::string extract_request_target(const std::string& http_request) {
     std::istringstream request_stream(http_request);
     std::string method, request_target, http_version;
@@ -46,40 +85,25 @@ std::string receive_data(int sockfd, sockaddr_in& client_addr, socklen_t& client
 
 
 int main(int argc, char **argv) {
-  // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
-  }
-  
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
+  int port = 4221;
+  int backlog = 5;
+
+  int server_fd = create_server_socket(port);
+  if (server_fd < 0) return 1;
+
+  if (!bind_socket(server_fd, port)) {
+    close(server_fd);
     return 1;
   }
-  
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(4221);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 4221\n";
+
+  if (!start_listening(server_fd, backlog)) {
+    close(server_fd);
     return 1;
   }
-  
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-  
+
   struct sockaddr_in client_addr;
   socklen_t client_addr_len = sizeof(client_addr);
   
@@ -113,8 +137,6 @@ int main(int argc, char **argv) {
 
   }
 
-  std::cout << "Client connected\n";
-  
   close(server_fd);
 
   return 0;
